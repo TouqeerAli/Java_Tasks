acf027ab7b8b590a75c36ed1ec9555b8775ad6e7
Touqeer Ali Soomro <engr.touqeerali@gmail.com>
2023-02-23 18:26:21 +0500

    making Inheritance available

---
+++ Inheritance/Car.java
@@ -0,0 +1,24 @@
+public class Car extends Vehicle{
+
+	public void carDrives(){
+		System.out.println("Car is Driving");
+	}
+	
+}
+
+
+
+/*
+public Car(String model,String color, String plateNo){
+	this.model=model;
+	this.color=color;
+	this.plateNo=plateNo;
+	}
+	
+public static void main(String[] args){
+	Car tesla = new Car("Model S","Black","xyz12");
+	System.out.println(tesla.model);
+	System.out.println(tesla.color);
+	System.out.println(tesla.plateNo);
+	
+	}*/
---
+++ Inheritance/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle{
+	//String model;
+	//String color;
+	//String plateNo;
+	public void vehicleDrives(){
+		System.out.println("Vehicle is Driving");
+	}
+	
+}
